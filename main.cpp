a951736bab01aeae523331aefbe1817a1a072bc7
Calvinbullock <calvinnbullock@gamil.com>
2023-08-28 18:58:15 -0700

    basic player movement implementation

--- main.cpp
+++ main.cpp
@@ -8,10 +8,15 @@ int main()
 {
     float windowWidth = 400;
     float windowHeight = 400;
+    int playerX = -200;
+    int playerY = -50;
+    int moveAmt = 10; // the amount of pixels the player moves with each key press.
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "NewLife");
 
     GameWorld gameWorld = GameWorld();
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
 
     // Main event / game loop
     while (window.isOpen())
@@ -21,7 +26,42 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+            else if (event.type == sf::Event::KeyPressed) // Player key entry.
+            {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+                {
+                    std::cout << "you pushed W" << std::endl;
+                    playerY += moveAmt;
+                    if (!playerTexture.loadFromFile("images/protag-up-stand.png"))
+                        return -1;
+                }
+                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                {
+                    std::cout << "you pushed A" << std::endl;
+                    playerX += moveAmt;
+                    if (!playerTexture.loadFromFile("images/protag-up-stand.png"))
+                        return -1;
+                }
+                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                {
+                    std::cout << "you pushed S" << std::endl;
+                    playerY -= moveAmt;
+                    if (!playerTexture.loadFromFile("images/protag-down-stand.png"))
+                        return -1;
+                }
+                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                {
+                    std::cout << "you pushed D" << std::endl;
+                    playerX -= moveAmt;
+                    if (!playerTexture.loadFromFile("images/protag-up-stand.png"))
+                        return -1;
+                }
+                // set the sprite for the directional texture.
+                playerSprite.setTexture(playerTexture);
+            }
         }
 
         window.clear();
@@ -35,7 +75,10 @@ int main()
             }
         }
 
+        window.draw(playerSprite);
+        playerSprite.setTextureRect(sf::IntRect(playerX, playerY, 500, 400));
         window.display();
+        // std::cout << "redraw" << std::endl;
     }
 
     return 0;
