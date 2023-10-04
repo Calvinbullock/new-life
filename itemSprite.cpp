85217d33face1eac80fc95daa156d30db2c8679a
Calvinbullock <calvinnbullock@gmail.com>
2023-10-03 19:22:51 -0700

    itemSprite classes added

---
+++ itemSprite.cpp
@@ -0,0 +1,32 @@
+#include "itemSprite.h"
+
+#include <iostream> // DEBUGing
+#define DEBUG std::cout << " DEBUG: L" << __LINE__ << " "
+
+ItemSprite::ItemSprite(std::string texturePath, float startX, float startY) : texture(),
+                                                                                  sprite(),
+                                                                                  itemBoundery()
+{
+    if (!SetUpSprite(texturePath))
+        return;
+
+    sprite.setPosition(sf::Vector2f(startX, startY));
+    itemBoundery = sprite.getGlobalBounds();
+}
+
+bool ItemSprite::SetUpSprite(std::string texturePath)
+{
+    if (!texture.loadFromFile(texturePath))
+        return false;
+
+    texture.setSmooth(true);
+    sprite.setTexture(texture);
+
+    return true;
+}
+
+void ItemSprite::SetItemXY(float x, float y)
+{
+    sprite.setPosition(sf::Vector2f(x, y));
+}
+
