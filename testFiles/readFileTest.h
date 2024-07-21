14c53dbf908cc1593ae0de8a825a26fc56cf3d0c
Calvinbullock <calvinnbullock@gmail.com>
2024-07-21 09:28:43 -0600

    added more tests to readFileTest

--- testFiles/readFileTest.h
+++ testFiles/readFileTest.h
@@ -1,12 +1,18 @@
 
+/* ================================================
+* Author: Calvin Bullock
+*
+* This is a testing file for readFile.h
+================================================ */
+
 #include "../readFile.h"
 #include <iostream>
 #include <string>
 #include <vector>
 
 /* ================================================
-*  TEST SPLIT LINE
-*  
+*  TEST SPLIT LINE - SIMPLE
+*
 *  CASE: simple split
 *  Input:  line = "M,tex/path,name"
 *  Output: data = ["M", "tex/path", "name"]
@@ -15,22 +21,73 @@ inline int TestSplitLine_Simple() {
     // setup
     std::string line = "M,tex/path,name";
     vector<string> data = SplitLine(',', line);
-      
+
     // check
-    if (data[0] != "M")
+    if (data[0] != "M") {
         cout << "fail" << data[0] << endl;
+        return 1;
+    }
 
-    if (data[1] != "tex/path")
+    if (data[1] != "tex/path") {
         cout << "fail" << endl;
+        return 1;
+    }
 
-    if (data[2] != "name")
+    if (data[2] != "name") {
         cout << "fail" << endl;
+        return 1;
+    }
+
+    return 0;
+}
+
+/* ================================================
+*  TEST SPLIT LINE - LONG
+*
+*  CASE: Long split
+*  Input:  line = "B,tex/path,name,yop,hop"
+*  Output: data = [ "M", "tex/path", "name", "yop", "hop" ]
+================================================ */
+inline int TestSplitLine_Long() {
+    // setup
+    std::string line = "B,tex/path,name,yop,hop";
+    vector<string> data = SplitLine(',', line);
+
+    // check
+    if (data[0] != "B") {
+        cout << "fail" << data[0] << endl;
+        return 1;
+    }
+
+    if (data[1] != "tex/path") {
+        cout << "fail" << endl;
+        return 1;
+    }
+
+    if (data[2] != "name") {
+        cout << "fail" << endl;
+        return 1;
+    }
+
+    if (data[3] != "yop") {
+        cout << "fail" << endl;
+        return 1;
+    }
+
+    if (data[4] != "hop") {
+        cout << "fail" << endl;
+        return 1;
+    }
 
     return 0;
 }
 
 inline void Run_ReadFileTests() {
-   if (TestSplitLine_Simple() == 1)
-      cout << "TestSplitLine_Simple - fail" << endl; 
+    if (TestSplitLine_Simple() == 1)
+        cout << "TestSplitLine_Simple - fail" << endl;
 
-} 
+    if (TestSplitLine_Long() == 1)
+        cout << "TestSplitLine_Long - fail" << endl;
+}
+
+
