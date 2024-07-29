0efeef8fe3a1cc51a5413d8f37268aca967da330
Calvinbullock <calvinnbullock@gmail.com>
2024-07-29 16:17:32 -0700

    formatting

--- items/doorItem.h
+++ items/doorItem.h
@@ -12,29 +12,27 @@
 *  the door tile.
 ================================================ */
 
+#include "../gameLevel.h"
 #include "../item.h"
-#include "../tileMap.h"
 
 class DoorItem : public Item {
 
 public:
-
-    /*ItemDoor(std::string texturePath, float startX, float startY, bool isLocked)*/
+    /*ItemDoor(std::string texturePath, float startX, float startY, bool
+     * isLocked)*/
     /*    : Item(texturePath, startX, startY), isLocked(isLocked) {}*/
 
-    DoorItem(std::string texturePath, float startX, float startY)
-        : Item(texturePath, startX, startY) {}
+    DoorItem(std::string texturePath, float posX, float posY, bool isLocked)
+        : Item(texturePath, posX, posY), isLocked(isLocked) {}
 
-    void itemAction() {
-      
-   }
+    void itemAction() {}
 
 protected:
-
     bool isLocked;
-    // GameLevel side1;
-    std::vector<int> posSide1;
-    // GameLevel side2;
-    std::vector<int> posSide2;
 
+    GameLevel side1;
+    std::vector<int> posSide1;
+
+    GameLevel side2;
+    std::vector<int> posSide2;
 };
