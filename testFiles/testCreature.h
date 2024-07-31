c774e96275acd4e95332c144dffcf687d03cbbf9
Calvinbullock <calvinnbullock@gmail.com>
2024-07-30 19:23:36 -0700

    testUpdateHealth_hp10 added

--- testFiles/testCreature.h
+++ testFiles/testCreature.h
@@ -7,13 +7,14 @@
 #pragma once
 
 #include "../creatures/creature.h"
+#include <cassert>
 #include <iostream>
 #include <iterator>
 
 class TestCreature {
 
 public:
-   TestCreature() {}
+    TestCreature() {}
 
     /* ================================================
      * RUN TEST CREATURES
@@ -21,10 +22,33 @@ public:
      * Think of this like main
     ================================================ */
     void Run_TestCreatures() {
-        TestUpdateHealth();
+        TestUpdateHealth_hp10();
         std::cout << "Run_TestCreatures - Pass" << std::endl;
     }
 
 private:
-    void TestUpdateHealth() {}
+    /* ================================================
+     * Test Update Health - hp 10
+     *
+     * input:
+     *   health  = 10, isAlive = true, updateHealth = -10
+     *
+     * Output:
+     *   health  = 0, isAlive = false
+    ================================================ */
+    void TestUpdateHealth_hp10() {
+      // set up
+      Creature c;
+      c.isAlive = true;
+      c.currentHealth = 10;
+
+      // action
+      c.UpdateHealth(-10);
+
+      // check
+      assert(c.currentHealth == 0);
+      assert(c.isAlive == false);
+
+      // clean up
+   }
 };
